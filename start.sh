18746703e28dccf1a711d6b4bc125f1c690e0af6
AidenMason <101789306+AidenMason@users.noreply.github.com>
2022-04-20 01:21:13 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
 # configs
-AUUID=2c835310-dddd-4b3d-8e9b-7841c897d070
-CADDYIndexPage=https://github.com/vvv1/saide/raw/main/3D.zip
-CONFIGCADDY=https://raw.githubusercontent.com/ddd/ao/master/etc/Caddyfile
-CONFIGXRAY=https://raw.githubusercontent.com/ddd/ao/master/etc/xray.json
+AUUID=8b73f727-f9a5-498c-8856-b206113eb950
+CADDYIndexPage=https://github.com/AidenMason/weiz2/raw/main/caddy.zip
+CONFIGCADDY=https://raw.githubusercontent.com/AidenMason/xhct3/master/etc/Caddyfile
+CONFIGXRAY=https://raw.githubusercontent.com/AidenMason/xhct3/master/etc/xray.json
 ParameterSSENCYPT=chacha20-ietf-poly1305
-StoreFiles=https://raw.githubusercontent.com/ddd/ao/master/etc/StoreFiles
+StoreFiles=https://raw.githubusercontent.com/AidenMason/xhct3/master/etc/StoreFiles
 #PORT=4433
 mkdir -p /etc/caddy/ /usr/share/caddy && echo -e "User-agent: *\nDisallow: /" >/usr/share/caddy/robots.txt
 wget $CADDYIndexPage -O /usr/share/caddy/index.html && unzip -qo /usr/share/caddy/index.html -d /usr/share/caddy/ && mv /usr/share/caddy/*/* /usr/share/caddy/
